e4647ddcedb14f4cf80a5c5426cc6fdb5c45c4f5
Norman Heino <norman.heino@gmail.com>
2013-03-26 12:08:43 +0100

    Fix jshint warnings.

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-var events = require('events');
-var bindings = require(__dirname + '/build/Release/bindings');
-
+var events = require('events'),
+    util = require('util'),
+    bindings = require(__dirname + '/build/Release/bindings');
 
 function inherits(target, source) {
-    for (var k in source.prototype)
-        target[k] = source.prototype[k];
+    for (var key in source.prototype) {
+        target[key] = source.prototype[key];
+    }
 }
 
-exports.newParser = function(mimeType, cb) {
-    if (undefined == cb) {
+exports.newParser = function (mimeType, cb) {
+    if (undefined === cb) {
         var parser = bindings.newParser(mimeType);
         inherits(parser, events.EventEmitter);
         return parser;
     } else {
-        var res = bindings.newParser(mimeType, function(parser) {
+        var res = bindings.newParser(mimeType, function (parser) {
             inherits(parser, events.EventEmitter);
             cb(parser);
         });
     }
-}
+};
 
 
-exports.newSerializer = function(mimeType) {
+exports.newSerializer = function (mimeType) {
     var serializer = null;
 
-    if (undefined == mimeType) {
+    if (undefined === mimeType) {
         serializer = bindings.newSerializer();
     } else {
         serializer = bindings.newSerializer(mimeType);
@@ -33,4 +34,4 @@ exports.newSerializer = function(mimeType) {
     inherits(serializer, events.EventEmitter);
 
     return serializer;
-}
+};
