a7cf7f6866b741192af9e5ed61ff0f916a199b5e
Norman Heino <norman.heino@gmail.com>
2013-03-26 12:09:10 +0100

    Add NODE_MODULE definition.

--- src/bindings.cc
+++ src/bindings.cc
@@ -22,8 +22,9 @@
 using namespace v8;
 using namespace node;
 
-/* extern "C" to prevent name mangling */
-extern "C" void init(Handle<Object> target) {
+void Initialize(Handle<Object> target) {
   NODE_SET_METHOD(target, "newParser", Parser::Initialize);
   NODE_SET_METHOD(target, "newSerializer", Serializer::Initialize);
 }
+
+NODE_MODULE(bindings, Initialize);
